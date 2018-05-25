19f784a59df182ee0c08bc53c71d2847b5d1ec4b
Rostyslav Khoptiy <ros65536@gmail.com>
2018-05-25 23:42:03 +0100

    improve search looks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 
+import './App.css'
+
 class App extends Component {
     render() {
         return (
             <div>
+                {/* Header */}
                 <Navbar inverse fixedTop>
                     <Navbar.Header>
                         <Navbar.Brand>
@@ -12,7 +15,7 @@ class App extends Component {
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
-                    <form class="navbar-form navbar-right" role="search">
+                    <form id="header-search" class="navbar-form navbar-right" role="search">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Search" />
                         </div>
@@ -21,9 +24,11 @@ class App extends Component {
                         </button>
                     </form>
                 </Navbar>
+                {/* Body */}
                 <Jumbotron>
                     <Grid>
                         <h1>Hiya</h1>
+                        <p> Hi</p>
                     </Grid>
                 </Jumbotron>
             </div>
